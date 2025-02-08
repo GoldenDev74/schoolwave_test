38e0351b0593ba28cb5ca84ccc7be63e38dee9d7
@GoldenDev74 <sessoukodjogolden@gmail.com>
2025-02-08 14:30:10 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AnneeScolaireController;
 use App\Http\Controllers\CategorieMatiereController;
@@ -37,48 +36,15 @@ use App\Http\Controllers\UserProfilController;
 use App\Http\Controllers\CorrespondanceController;
 use App\Http\Controllers\EnseignantControleController;
 use App\Http\Controllers\SuiviCoursController;
-=======
 use App\Http\Controllers\ControleController;
 use App\Http\Controllers\CorrespondanceController;
->>>>>>> 9557ee469115dda5e8f36788a04f70f84d7c19fc
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-<<<<<<< HEAD
-| Ici se trouvent les routes web de l'application. Ces routes sont
-| chargées via le RouteServiceProvider au sein du groupe "web".
-|
-*/
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Home et authentification
-Route::get('/', [HomeController::class, 'index'])->name('home');
 Auth::routes();
-
 
 Route::delete('affectation-matieres/{id}', [AffectationMatiereController::class, 'destroy'])
      ->name('affectation-matieres.destroy');
-
-// Ressources principales
-Route::resource('anneeScolaires', AnneeScolaireController::class);
-Route::resource('categorieMatieres', CategorieMatiereController::class);
-Route::resource('diplomes', DiplomeController::class);
-Route::resource('enseignants', EnseignantController::class);
-Route::resource('examens', ExamenController::class);
-Route::resource('fileres', FilereController::class);
-Route::resource('liens', LiensController::class);
-Route::resource('matieres', MatiereController::class);
-Route::resource('salles', SallesController::class);
-Route::resource('sexes', SexeController::class);
-Route::resource('typeCours', TypeCoursController::class);
-Route::resource('typeExamens', TypeExamenController::class);
-Route::resource('typeHoraires', TypeHoraireController::class);
-Route::resource('userLiens', UserLiensController::class);
-Route::resource('users', UsersController::class);
-//Route::resource('affectation-matieres', App\Http\Controllers\AffectationMatiereController::class);
-
 
 // Groupe de routes pour AffectationMatiere
 Route::prefix('affectationMatieres')->group(function () {
@@ -133,20 +99,7 @@ Route::resource('correspondances', CorrespondanceController::class);
 Route::resource('suivi_cours', SuiviCoursController::class);
 
 Route::get('controles/details', [ControleController::class, 'details'])
-     ->name('controles.details')
-     ->middleware('auth'); // ✅ Sécurité basique
-=======
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
+     ->name('controles.details');
 
 Route::get('testMail', [App\Http\Controllers\HomeController::class,'testMail'])->name('testMail');
 Route::resource('anneeScolaires', App\Http\Controllers\AnneeScolaireController::class);
@@ -164,20 +117,7 @@ Route::resource('typeExamens', App\Http\Controllers\TypeExamenController::class)
 Route::resource('typeHoraires', App\Http\Controllers\TypeHoraireController::class);
 Route::resource('userLiens', App\Http\Controllers\UserLiensController::class);
 Route::resource('users', App\Http\Controllers\UsersController::class);
-//Route::resource('affectationMatieres', App\Http\Controllers\AffectationMatiereController::class);
-Route::resource('classes', App\Http\Controllers\ClasseController::class);
-Route::resource('horaires', App\Http\Controllers\HoraireController::class);
 Route::resource('jourSemaines', App\Http\Controllers\JourSemaineController::class);
-
-
-Route::resource('modeAffectations', App\Http\Controllers\ModeAffectationController::class);
-Route::resource('lienParentEleves', App\Http\Controllers\LienParentEleveController::class);
-Route::resource('pays', App\Http\Controllers\PaysController::class);
-Route::resource('parents', App\Http\Controllers\ParentController::class);
-Route::resource('eleves', App\Http\Controllers\EleveController::class);
-Route::resource('meseleves', App\Http\Controllers\MesElevesController::class);
-Route::resource('effectifs', App\Http\Controllers\EffectifController::class);
-Route::resource('controles', App\Http\Controllers\ControleController::class);
 
 Route::resource('affectation-matieres', App\Http\Controllers\AffectationMatiereController::class);
 
@@ -192,18 +132,9 @@ Route::delete('affectationMatieres/{id}', [App\Http\Controllers\AffectationMatie
 Route::get('/controles/{enseignantId}/classes', [ControleController::class, 'getClassesByEnseignant'])->name('controles.classes');
 Route::get('/controles/enseignants/{classeId}', [ControleController::class, 'getEnseignantsByClasse'])->name('controles.enseignantsByClasse');
 Route::get('controles/eleves/{classeId}', [ControleController::class, 'getElevesByClasse'])->name('controles.elevesByClasse');
-
-//Route::post('/controles/details', [ControleController::class, 'getControleDetails']);
 Route::get('/controles/details', [ControleController::class, 'details'])->name('controles.details');
 Route::get('controles/{controle}', [ControleController::class, 'show'])->name('controles.show');
-
-Route::resource('typepersonnels', App\Http\Controllers\TypepersonnelController::class);
-Route::resource('profils', App\Http\Controllers\ProfilController::class);
-Route::resource('userProfils', App\Http\Controllers\UserProfilController::class);
-Route::resource('correspondances', App\Http\Controllers\CorrespondanceController::class);
-Route::resource('suivi_cours', App\Http\Controllers\SuiviCoursController::class);
-
->>>>>>> 9557ee469115dda5e8f36788a04f70f84d7c19fc
+Route::get('controles/{controle}', [ControleController::class, 'show'])->name('controles.show');
 
 //fields dynamique pour correspondance
 Route::any('/user-classes', [CorrespondanceController::class, 'getUserClasses']);
@@ -211,7 +142,5 @@ Route::get('/get-recipients', [\App\Http\Controllers\CorrespondanceController::c
 Route::get('correspondances/get-recipients', [App\Http\Controllers\CorrespondanceController::class, 'getRecipients'])
      ->name('correspondances.getRecipients');
 
-<<<<<<< HEAD
-Route::get('controles/{controle}', [ControleController::class, 'show'])->name('controles.show');
-=======
->>>>>>> 9557ee469115dda5e8f36788a04f70f84d7c19fc
+
+
